05c3df1deb0c31b678221408a3bd526d40ef64f6
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-13 21:46:39 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+// dynamic pages
+//set view (pug, ejs, hbs)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
+
+// static pages
+// middleware
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+//routes
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to Home'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        sucess: false,
+        message: 'unable to handle the request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
